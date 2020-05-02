ad8f1ea991bdb3de9f1720e5d8a17f973ba67e55
CUDDLER <bagldya@gmail.com>
2020-05-02 13:20:29 +0300

    add request to coronavirus monitor and output in message

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from bot.handler import MessageHandler
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -11,7 +13,20 @@ TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 
 def message_cb(bot, event):
-    bot.send_text(chat_id=event.from_chat, text=event.text)
+
+    text=requests.get('https://api.thevirustracker.com/free-api',params={'global': 'stats'},)
+    if text.status_code != 200:
+        # This means something went wrong.
+        print('ВСЕ МИМО')
+    data=text.json()
+    data=data['results'][0]
+    totalCases=data['total_cases']
+    totalRecovered=data['total_recovered']
+    print(totalCases,totalRecovered)
+    covidinfo='Всего заразившихся %s!' % totalCases
+    print(covidinfo)
+    bot.send_text(chat_id=event.from_chat, text=covidinfo)
+
 
 bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
 bot.start_polling()
