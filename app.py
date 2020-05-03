2ca62b21230c4f8db10abf7e934dbfe6510a1d1a
Tvo9Mamka <prawad@mail.ru>
2020-05-03 22:37:54 +0500

    Добавлен вывод статистики по миру и большенству стран за весь период или сегодня. Добавлена команда /info, вызывающая подсказку по выводу статистики

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import json
 import requests
-
+import country
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 TOKEN = os.getenv('TOKEN')
 
 bot = Bot(token=TOKEN)
-
 
 
 # class tests(user):
@@ -22,21 +21,93 @@ def startmes(bot, event):
     if event.data['msgId']['text'] == '/start':
         bot.send_text(chat_id=event.from_chat, text='Привет')
 
+
 def message_cb(bot, event):
-    text=requests.get('https://api.thevirustracker.com/free-api',params={'global': 'stats'},)
-    if text.status_code != 200:
-        # This means something went wrong.
-        print('ВСЕ МИМО')
-    data=text.json()
-    data=data['results'][0]
-    totalCases=data['total_cases']
-    totalRecovered=data['total_recovered']
-   # print(totalCases,totalRecovered)
-    covidinfo='Всего заразившихся %s!' % totalCases
-   # print(covidinfo)
-    #print(event.from_chat)
-    print(event)
-    bot.send_text(chat_id=event.from_chat, text=covidinfo)
+    back = event.text.split()
+    if back[0].lower() == 'всего' and len(back) > 1:
+        if back[1].lower() == 'мир':
+            wed_site = 'https://api.thevirustracker.com/free-api?global=stats'
+            text = requests.get(wed_site)
+            if text.status_code != 200:
+                # This means something went wrong.
+                print('ВСЕ МИМО')
+            else:
+                data = text.json()
+                totalCases = data['results'][0]['total_cases']
+                totalRecovered = data['results'][0]['total_recovered']
+                total_deaths = data['results'][0]['total_deaths']
+                covidinfo = 'Мир' + \
+                            '\nВсего заразившихся %s' % totalCases + \
+                            '\nВсего выздоровело %s' % totalRecovered + \
+                            '\nВсего умерло %s' % total_deaths
+                curUser = event.data['from']['userId']
+                bot.send_text(chat_id=curUser, text=covidinfo)
+        else:
+            coun = back[1][0].upper() + back[1][1:].lower()
+            wed_site = 'https://api.thevirustracker.com/free-api?countryTotal=' + country.country[coun]
+            text = requests.get(wed_site)
+            if text.status_code != 200:
+                # This means something went wrong.
+                print('ВСЕ МИМО')
+            else:
+                data = text.json()
+                totalCases = data['countrydata'][0]['total_cases']
+                totalRecovered = data['countrydata'][0]['total_recovered']
+                total_deaths = data['countrydata'][0]['total_deaths']
+                covidinfo = '%s:' % coun + \
+                            '\nВсего заразившихся %s' % totalCases + \
+                            '\nВсего выздоровело %s' % totalRecovered + \
+                            '\nВсего умерло %s' % total_deaths
+                curUser = event.data['from']['userId']
+                bot.send_text(chat_id=curUser, text=covidinfo)
+    elif back[0].lower() == 'сегодня' and len(back) > 1:
+        if back[1].lower() == 'мир':
+            wed_site = 'https://api.thevirustracker.com/free-api?global=stats'
+            text = requests.get(wed_site)
+            if text.status_code != 200:
+                # This means something went wrong.
+                print('ВСЕ МИМО')
+            else:
+                data = text.json()
+                todayCases = data['results'][0]['total_new_cases_today']
+                todaydeaths = data['results'][0]['total_new_deaths_today']
+                covidinfo = 'Мир' + \
+                            '\nСегодня заразившихся %s' % todayCases + \
+                            '\nСегодня умерло %s' % todaydeaths
+                curUser = event.data['from']['userId']
+                bot.send_text(chat_id=curUser, text=covidinfo)
+        else:
+            coun = back[1][0].upper() + back[1][1:].lower()
+            wed_site = 'https://api.thevirustracker.com/free-api?countryTotal=' + country.country[coun]
+            text = requests.get(wed_site)
+            if text.status_code != 200:
+                # This means something went wrong.
+                print('ВСЕ МИМО')
+            else:
+                data = text.json()
+                print(data)
+                todayCases = data['countrydata'][0]['total_new_cases_today']
+                todaydeaths = data['countrydata'][0]['total_new_deaths_today']
+                covidinfo = '%s:' % coun + \
+                            '\nСегодня заразившихся %s' % todayCases + \
+                            '\nСегодня умерло %s' % todaydeaths
+                curUser = event.data['from']['userId']
+                bot.send_text(chat_id=curUser, text=covidinfo)
+    else:
+        text = requests.get('https://api.thevirustracker.com/free-api', params={'global': 'stats'}, )
+        if text.status_code != 200:
+            # This means something went wrong.
+            print('ВСЕ МИМО')
+        data = text.json()
+        data = data['results'][0]
+        totalCases = data['total_cases']
+        totalRecovered = data['total_recovered']
+        # print(totalCases,totalRecovered)
+        covidinfo = 'Всего заразившихся %s!' % totalCases
+        # print(covidinfo)
+        # print(event.from_chat)
+        bot.send_text(chat_id=event.from_chat, text=covidinfo)
+
 
 def runTests(bot, event):
     print('Hai')
@@ -49,39 +120,19 @@ def runTests(bot, event):
 
 
 def testcommands(bot, event):
-    print(bot)
-    print(event)
-    print(event.data['callbackData'])
-    strToParse=event.data['callbackData'].split('_')
-    count=strToParse[1]
-    stage=strToParse[0]
-    print(count, stage)
-    log='Count: %s Stage: %s' % (count, stage)
-    curUser=event.data['from']['userId']
-    print('Log',log, '          Stage true?', stage == '0')
-    bot.send_text(chat_id=curUser, text=log)
-    msg_id=event.data['message']['msgId']
-    print(msg_id)
-    if stage == '0':
-        print('Hallo')
-        bot.edit_text(chat_id=curUser, msg_id=msg_id, text="edited text",
-                      inline_keyboard_markup="{}".format(json.dumps([[
-                {"text": "Action 1", "callbackData": "1_2"},
-                {"text": "Action 2", "callbackData": "1_2"},
-            ]])))
-    if stage == '1':
-        bot.edit_text(chat_id=curUser, msg_id=msg_id, text="edited text",
-                      inline_keyboard_markup="{}".format(json.dumps([[
-                          {"text": "Action 1", "callbackData": "1_2"},
-                          {"text": "Action 2", "callbackData": "1_2"},
-                      ]])))
-   if stage == '2':
+    pass
 
+
+def stat(bot, event):
+    bot.send_text(chat_id=event.from_chat,
+                  text="Для получения статистики о коронавирусе введите период(всего или сегодня) "
+                       "и место(страна или мир). Например: сегодня Россия, всего мир")
 
 
 bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
-bot.dispatcher.add_handler(CommandHandler(command='start',callback=startmes))
-bot.dispatcher.add_handler(CommandHandler(command='gettest',callback=runTests))
+bot.dispatcher.add_handler(CommandHandler(command='info', callback=stat))
+bot.dispatcher.add_handler(CommandHandler(command='start', callback=startmes))
+bot.dispatcher.add_handler(CommandHandler(command='gettest', callback=runTests))
 bot.dispatcher.add_handler(BotButtonCommandHandler(callback=testcommands))
 bot.start_polling()
 bot.idle()
